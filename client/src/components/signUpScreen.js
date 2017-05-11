939145a3687906967136c88ac10b327a89f8dbaa
JacobHughes <yacolt@gmail.com>
2017-05-11 11:33:12 +1000

    APEC-3 = Added sign up page design as per spec

--- client/src/components/signUpScreen.js
+++ client/src/components/signUpScreen.js
@@ -6,10 +6,12 @@ import AppBarMain from '../components/AppBarMain';
 import Paper from 'material-ui/Paper';
 import StarRating from '../components/starRating';
 import RaisedButton from 'material-ui/RaisedButton';
-import {indigo,white} from './apecConnectTheme';
+import {indigo,white,red} from './apecConnectTheme';
 import Logo from '../common/assets/APEC-CONNECT-LOGO.svg';
 import {Link} from 'react-router';
 import actions from 'state/actions';
+import TextField from 'material-ui/TextField';
+import Background from '../common/assets/bg-bottom.png';
 
 /***
 
@@ -20,31 +22,32 @@ This is the basic elements for a main view in the app.
 const paperStyle = {
   padding: 10,
   textAlign: 'center',
-  height: '100vh'
+  height: '100vh',
+  overflow: 'hidden',
 };
 
-const getPaidStyle = {
-  width: '50%',
+const divStyle = {
+   height: '100%',
+   backgroundImage: `url(${Background})`,
+   backgroundSize: 'cover',
+   color: `${white}`,
+   position: 'relative',
 };
 
-const payStyle = {
-  width: '50%',
-};
-
-const inboxStyle ={
-};
-
-const businessNameStyle ={
-  fontSize: 48,
-};
-
-const userDetailsStyle = {
-  fontSize: 20,
+const textStyle = {
+  color: `${white}`,
+  width: '75%',
+  margin: '0 auto',
+  position: 'relative',
 };
 
 const logoStyle ={
   width: '100%',
   maxHeight: '150px',
+};
+
+const btnStyle ={
+  width: '100%',
 };
 
 {/** User information that will be pulled from the database **/}
@@ -82,9 +85,35 @@ export default class SignUpScreen extends React.Component {
           <br />
           <br />
 
-          <div> Hello please sign up </div>
-          <RaisedButton label='Sign Up' onTouchTap={this.handleSignIn}/>
-          <br />
+          <div style={divStyle}>
+            <div style={textStyle}>
+              <TextField
+                hintText="Business Name"
+                errorText="This field is required"
+                floatingLabelText='Business Name'
+              />
+
+              <div style={textStyle}> OR </div>
+              <TextField
+                hintText="Email Address"
+                floatingLabelText="Email Address"
+                type="email"
+              />
+
+              <div style={textStyle}> OR </div>
+              <TextField
+                hintText="Mobile Phone Number"
+                floatingLabelText="Mobile Phone Number"
+                type="number"
+              />
+              <div style={textStyle}> OR </div>
+              <br />
+              <RaisedButton style={btnStyle} label='Social Media' primary={true}/>
+            </div>
+            <br/>
+            <RaisedButton style={btnStyle} label='Sign Up' onTouchTap={this.handleSignIn} backgroundColor={red} labelColor={white}/>
+            <br />
+          </div>
         </Paper>
       </div>
     );
