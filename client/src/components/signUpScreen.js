3e2d9a167a573df587e80fbb86cbde836a2e6a1c
Haolin Jiang <hljiang@agiledigital.com.au>
2017-05-12 15:02:10 +1000

    APEC-3 = Clean up.

--- client/src/components/signUpScreen.js
+++ client/src/components/signUpScreen.js
@@ -106,7 +106,7 @@ export default class SignUpScreen extends React.Component {
 
           <div style={divStyle}>
             <div style={textStyle}>
-              <TextField required hintText="Business Name" errorText="This field is required" floatingLabelText='Business Name' onChange={(event) => this.onParticipantInfoChange({businessName: event.target.value})} value={participant.businessName}/>
+              <TextField hintText="Business Name" errorText="This field is required" floatingLabelText='Business Name' onChange={(event) => this.onParticipantInfoChange({businessName: event.target.value})} value={participant.businessName}/>
 
               <div style={textStyle}>
                 AND
