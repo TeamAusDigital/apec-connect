a2868d90b1cac2dea2a4e0bdab171cfee8534c72
Rory Stokes <rmstokes@agiledigital.com.au>
2017-05-17 12:32:22 +1000

    APEC-29 Handle errors in announcement fetching.

--- server/modules/api/app/org/ausdigital/apecconnect/announcements/controllers/AnnouncementsController.scala
+++ server/modules/api/app/org/ausdigital/apecconnect/announcements/controllers/AnnouncementsController.scala
@@ -5,6 +5,7 @@ import javax.inject.Inject
 import au.com.agiledigital.rest.controllers.transport.JsonApiResponse
 import org.ausdigital.apecconnect.announcements.services.AnnouncementService
 import play.api.mvc.{Action, AnyContent}
+import io.kanaka.monadic.dsl._
 
 import scala.concurrent.ExecutionContext
 
@@ -14,7 +15,7 @@ class AnnouncementsController @Inject() (
 
   def announcements(): Action[AnyContent] = Action.async { request =>
     for {
-      announcements <- announcementService.getAnnouncements
+      announcements <- announcementService.getAnnouncements  ?| JsonApiResponse.internalServerErrorResponse(s"Failed to fetch attachments.")
     } yield JsonApiResponse.buildResponse(s"Retrieved public announcements.", announcements)
   }
 }
